7313baa839196f5e153d89e982d203ec1bdcd956
Deep Shirke <shirkedeep360@gmail.com>
2025-09-18 13:56:03 +0000

    When I open the app on mobile, some of the components of the nav bar goe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <body className={cn('font-body antialiased')}>
         <Providers>
           <AuthWrapper>
-            <div className="relative z-10">{children}</div>
+            <div>{children}</div>
             <Toaster />
           </AuthWrapper>
         </Providers>
