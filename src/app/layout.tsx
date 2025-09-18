c3564351c9f9458a0eaa38f8fe43d63a0a383eb8
Deep Shirke <shirkedeep360@gmail.com>
2025-09-18 13:59:13 +0000

    It is still persistent.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <body className={cn('font-body antialiased')}>
         <Providers>
           <AuthWrapper>
-            <div>{children}</div>
+            <div className="flex-1 w-full">{children}</div>
             <Toaster />
           </AuthWrapper>
         </Providers>
--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import { useAuth } from "@/hooks/use-auth";
 import { Button } from "./ui/button";
